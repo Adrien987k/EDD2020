2b4c8dabd3f78aba5931a6b7dc40812a93f3686d
Adrien bardes <adrien.bardes@dbmail.com>
2020-03-28 20:28:49 +0100

    Resnet18

--- models/unetresnet.py
+++ models/unetresnet.py
@@ -15,8 +15,7 @@ class UNetResNet(nn.Module):
     def __init__(self, in_channel, n_classes):
         super().__init__()
 
-        print('load resnet 152...')
-        self.base_model = models.resnet152(pretrained=True)
+        self.base_model = models.resnet18(pretrained=True)
 
         self.base_layers = list(self.base_model.children())
 
